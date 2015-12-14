b679b9c2a5a28fc94a50abe444a0487bb8ad3c13
trmchale1 <trmchale1@github.com>
2015-12-14 17:48:17 -0500

    second update

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 #Fuck ISIS
  
 #We are Anonymous. We are Legion. We do not forgive. We do not forget. Expect us
-
+import sys  
+reload(sys)  
+sys.setdefaultencoding('utf-8')
 import requests
 from bs4 import BeautifulSoup
 
@@ -15,9 +17,14 @@ def printTweets(username):
     
     soup = BeautifulSoup(page.content, 'html.parser')
 #    tweets = soup.find_all('li','js-stream-item')
-    x = soup.prettify() 
-    x = x.encode('ascii', 'ignore').decode('ascii') 
-    print x
-#for i in data:
-printTweets('mohanad_202012')
+    twit_text = soup('p',{'class': 'js-tweet-text'}) 
+    
+#	message = twit_text[0].contents[0]
+    for j in range(len(twit_text)):
+	print twit_text[j].contents[0]
+    
+
+
+for i in data:
+	printTweets(i)
 
